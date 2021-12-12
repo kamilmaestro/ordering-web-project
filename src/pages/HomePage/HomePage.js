232b934a5a145d2d01ecb378e1220a2004255d7e
Kamil Marnik <ksdekamil@gmail.com>
2021-12-12 18:34:05 +0100

    router with home page added - skeleton

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homePage.css';
+import circle from '../../images/circle.png';
+import { PagePreview } from '../../components/PagePreview/PagePreview';
+import { HomeHeader } from '../../components/HomeHeader/HomeHeader';
+import { useHistory } from "react-router-dom";
+import { SIGN_IN_PAGE_URL } from '../../utils/urlProvider';
+
+export const HomePage = () => {
+
+
+	return (
+		<div>
+			hello
+		</div>
+	);
+};
